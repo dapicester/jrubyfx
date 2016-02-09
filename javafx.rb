8fbb0690f5b28e83a2c99dba808ca6470d259fb9
Paolo D'Apice <dapicester@gmail.com>
2016-02-09 16:07:59 +0800

    Initial commit

---
+++ javafx.rb
@@ -0,0 +1,24 @@
+require 'jrubyfx'
+
+class HelloJRubyFX < JRubyFX::Application
+  def start(stage)
+    uri = 'http://127.0.0.1:4567'
+    options = {
+      title: 'Black App',
+      width: 800,
+      height: 600
+    }
+    with stage, options do
+      layout_scene do
+        stack_pane do
+          web_view do
+            get_engine.load uri.to_s
+          end
+        end
+      end
+    end
+    stage.show
+  end
+end
+
+HelloJRubyFX.launch
